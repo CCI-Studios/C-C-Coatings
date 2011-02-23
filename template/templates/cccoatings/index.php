b103d6a127e4b8ba6158cdcb08b9c43dbc6305c0
Darren Murray <dmurray@ccistudios.com>
2011-02-23 17:15:09 -0500

    more site settings accomplished

--- template/templates/cccoatings/index.php
+++ template/templates/cccoatings/index.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
-
+<title>C&C Coatings</title>
 <html>
+
 <head>
-	
+	<link rel="stylesheet" href="/templates/cccoatings/css/template.css" />
 	
 </head>
 
 <body>
-	
-	
-	
-</body>
+		<div class="body"></div>
+		<div id="left"></div>
 
-<footer>
-	
-	
-	
-</footer>
+</body>
+</html>
