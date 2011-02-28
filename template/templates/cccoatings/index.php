88bfb2a86a1ae0278c37e75e66c53c99d9eddb5e
Darren Murray <dmurray@ccistudios.com>
2011-02-28 17:15:06 -0500

    Building structure for C&C Coatings

--- template/templates/cccoatings/index.php
+++ template/templates/cccoatings/index.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
-<title>C&C Coatings</title>
+
 <html>
 
 <head>
+	<jdoc:include type="head" />
 	<link rel="stylesheet" href="/templates/cccoatings/css/template.css" />
 	
 </head>
 
 <body>
-		<div class="body"></div>
-		<div id="left"></div>
+	<div id="wrapper">
 
+		<div id="top">
+			<div id="left">
+				<jdoc:include type="modules" name="left" style="xhtml"/>
+			</div>
+			<div id="right">
+				<jdoc:include type="modules" name="right" style="xhtml"/>
+			</div>
+		</div>
+		<div id="component"></div>
+		<div id="bottom"></div>
+		<div id="footer"></div>
+	</div>
 </body>
 </html>
+
+<div></div>
+
